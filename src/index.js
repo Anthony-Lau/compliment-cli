ef24ebeff3f8482eeeb7f2c1c077b483b78a5cfb
Anthony Lau <anthonycylau@gmail.com>
2019-09-01 13:10:13 -0500

    feat: adds lists of adjs, adverbs, and nouns

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 const {Command, flags} = require('@oclif/command')
+const adjs = require('./adjs')
+const nouns = require('./nouns')
+const adverbs = require('./adverbs')
 
 class ComplimentCliCommand extends Command {
   async run() {
     const {flags} = this.parse(ComplimentCliCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.js`)
+
+    const randomSelect = arr => {
+      return arr[Math.floor(Math.random() * arr.length)]
+    }
+
+    const sentenceGenerator = (noun, adj, adverb) => {
+      const templates = [
+        `Your ${noun} is looking ${adverb} ${adj} today!`,
+        `You have ${adverb} ${adj} ${noun}`,
+        `You are ${adverb} ${adj}`,
+
+      ]
+      this.log(randomSelect(templates))
+    }
+
+    // const name = flags.name
+
+    sentenceGenerator(randomSelect(nouns), randomSelect(adjs), randomSelect(adverbs))
   }
 }
 
 ComplimentCliCommand.description = `Describe the command here
-...
 Extra documentation goes here
 `
 
@@ -18,7 +36,7 @@ ComplimentCliCommand.flags = {
   version: flags.version({char: 'v'}),
   // add --help flag to show CLI version
   help: flags.help({char: 'h'}),
-  name: flags.string({char: 'n', description: 'name to print'}),
+  name: flags.string({char: 'n', description: 'name to compliment'}),
 }
 
 module.exports = ComplimentCliCommand
